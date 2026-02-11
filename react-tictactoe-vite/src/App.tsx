be2650c74558ece7445ef8fc51a8db64729de1db
Melike Eryılmaz (ETIYA) <melike.eryilmaz@etiya.com>
2026-02-11 14:55:21 +0300

    calculate winner func added

--- react-tictactoe-vite/src/App.tsx
+++ react-tictactoe-vite/src/App.tsx
@@ -12,18 +12,45 @@ function Square({value,onSquareClick}) {
   //return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 export default function Board() {
   const [xIsNext,setXISnext] = useState(true);
   const [squares,setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   function handleClick(index:number){
     /**const squares = [null, null, null, null, null, null, null, null, null];
        const nextSquares = ['X', null, null, null, null, null, null, null, null];
       // Now `squares` is unchanged, but `nextSquares` first element is 'X' rather than `null`
      */
+    if(squares[index] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
    if(xIsNext){
     nextSquares[index] = 'X';
-   }
+   }  
    else{
     nextSquares[index] = 'O';
    }
@@ -33,6 +60,7 @@ export default function Board() {
   }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
