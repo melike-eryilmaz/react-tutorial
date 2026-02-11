e0a1438dfaa170881d54558b4b72cb715671814f
Melike Eryılmaz (ETIYA) <melike.eryilmaz@etiya.com>
2026-02-11 15:45:26 +0300

    game development successfully completed

--- react-tictactoe-vite/src/App.tsx
+++ react-tictactoe-vite/src/App.tsx
@@ -82,19 +82,18 @@ export function Board({xIsNext,squares,onPlay}) {
 }
 
 export default function Game() {
-  const [xIsNext,setXISnext] = useState(true);
   const [history,setHistory] = useState([Array(9).fill(null)]); 
   const [currentMove,setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
   function handlePlay(nextSquares){
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXISnext(!xIsNext);
+
   }     
     function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXISnext(nextMove%2===0);
     
   }
    const moves = history.map((squares, move) => {
@@ -111,7 +110,7 @@ export default function Game() {
     );
   });
   return (
-    <div className="Game">
+    <div className="game">
       <div className="game-board">
       <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
